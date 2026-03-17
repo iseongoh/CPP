a73c5844ff943646af7f118e870b47b9e779fb57
iseongoh <lso04177@chosun.ac.kr>
2026-03-17 12:11:56 +0900

    first commit

---
+++ Week2/p.53 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+void asciiart() {
+    std::string lso = R"(
+  _         _____   ____  
+ | |       / ____| / __ \ 
+ | |      | (___  | |  | |
+ | |       \___ \ | |  | |
+ | |____   ____) || |__| |
+ |______| |_____/  \____/ 
+                          
+    )";
+}
+
+int main() {    
+    asciiart();
+
+    return 0;
+}
